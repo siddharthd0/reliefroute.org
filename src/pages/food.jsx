44fec3fb0f5beb0537bfdb517f04417e0c852644
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-10-08 20:00:13 -0500

    pages ++

---
+++ src/pages/food.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Landing from "../components/landing";
+import Navigation from "../components/navigation";
+
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+      <h1 className="text-4xl py-28 flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        Locate Food outside of War Zones
+      </h1>
+    </>
+  );
+}
