8d9d42822c763ff6ab53e708956bc2d7a0229ed2
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-10-10 16:27:30 -0500

    p

--- src/pages/api/verify.js
+++ src/pages/api/verify.js
@@ -1,24 +1,22 @@
-import { MongoClient, ObjectId } from 'mongodb';
-import nodemailer from 'nodemailer';
+import { MongoClient, ObjectId } from "mongodb";
+import nodemailer from "nodemailer";
 
 export default async (req, res) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { locationId, email } = req.body;
     try {
-      // Connect to MongoDB
       const client = await MongoClient.connect(process.env.MONGODB);
       const db = client.db();
-      const collection = db.collection('safeHavens');
-
-      // Convert locationId to ObjectId
+      const collection = db.collection("safeHavens");
       const objectId = new ObjectId(locationId);
 
-      // Update isVerified status
-      await collection.updateOne({ _id: objectId }, { $set: { isVerified: 'True' } });
+      await collection.updateOne(
+        { _id: objectId },
+        { $set: { isVerified: "True" } }
+      );
 
-      // Send verification email
       let transporter = nodemailer.createTransport({
-        service: 'gmail',
+        service: "gmail",
         auth: {
           user: process.env.EMAIL,
           pass: process.env.EMAIL_PASSWORD,
@@ -28,16 +26,16 @@ export default async (req, res) => {
       let info = await transporter.sendMail({
         from: '"War Help App" <hr@techoptimum.org>',
         to: email,
-        subject: 'Safe Haven Verification',
-        text: 'Your safe haven has been verified. If you need to update your information, please email us as soon as possible.',
+        subject: "Safe Haven Verification",
+        text: "Your safe haven has been verified. If you need to update your information, please email us as soon as possible.",
       });
 
       res.status(200).json({ success: true });
     } catch (error) {
-      console.error(error);  // Log error to console
+      console.error(error);
       res.status(500).json({ error: error.message });
     }
   } else {
-    res.status(405).end();  // Method Not Allowed
+    res.status(405).end();
   }
 };
