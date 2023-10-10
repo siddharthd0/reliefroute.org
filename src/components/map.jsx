e80d626df610ad69ae43b71187eaf80dda790f6f
Siddharth <96356209+siddharthd0@users.noreply.github.com>
2023-10-09 21:04:26 -0500

    a

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import Script from "next/script";
 
 function initMap() {
@@ -15,40 +14,8 @@ function initMap() {
     zoomControl: true,
     streetViewControl: false,
   });
-
-  const locations = [
-    {
-      position: { lat: 31.503, lng: 24.466 },
-      title: "Restaurant",
-      description: "test",
-    },
-  ];
-
-  let activeInfoWindow;
-
-  locations.forEach((location) => {
-    const marker = new window.google.maps.Marker({
-      position: location.position,
-      map,
-      title: location.title,
-    });
-
-    const infoWindow = new window.google.maps.InfoWindow({
-      content: `
-                <div style="max-width: 200px">
-                    <h2>${location.title}</h2>
-                    <p>${location.description}</p>
-                </div>
-            `,
-    });
-
-    marker.addListener("click", () => {
-      if (activeInfoWindow) activeInfoWindow.close();
-      infoWindow.open(map, marker);
-      activeInfoWindow = infoWindow;
-    });
-  });
 }
+
 if (typeof window !== "undefined") {
   window.initMap = initMap;
 }
@@ -66,3 +33,4 @@ const MapComponent = () => {
 };
 
 export default MapComponent;
+    
